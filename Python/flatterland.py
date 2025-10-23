3d8e95494fa3354213ee9e1a3d2eb832fc222c5e
Bryce Williams <bw1goal@gmail.com>
2025-10-22 20:26:06 -0400

    new problems

---
+++ Python/flatterland.py
@@ -0,0 +1,10 @@
+def flatterland():
+    people = int(input())
+    distance = int(input())
+
+    farthest_distance = (people - 1) * distance
+
+    print(farthest_distance)
+
+if __name__ == '__main__':
+    flatterland()
---
+++ Python/hipphipp.py
@@ -0,0 +1,9 @@
+def hipphipp():
+    name = input()
+    counter = int(input())
+
+    for i in range(counter):
+        print('Hipp hipp hurra, ' + name + '!') 
+
+if __name__ == '__main__':
+    hipphipp()
